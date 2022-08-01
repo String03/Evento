1b3d6ce5cd53878ef295121d66ce7dd5cf0fab94
String03 <40239185+String03@users.noreply.github.com>
2022-08-01 13:45:45 -0300

    Add files via upload

---
+++ Evento/Evento/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Evento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.txtCuenta = new System.Windows.Forms.TextBox();
+            this.txtSaldo = new System.Windows.Forms.TextBox();
+            this.txtDeposito = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtRetiro = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDeposito = new System.Windows.Forms.Button();
+            this.btnRetiro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cuenta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 230);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Saldo";
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(177, 49);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(148, 22);
+            this.txtCliente.TabIndex = 3;
+            // 
+            // txtCuenta
+            // 
+            this.txtCuenta.Location = new System.Drawing.Point(177, 125);
+            this.txtCuenta.Name = "txtCuenta";
+            this.txtCuenta.Size = new System.Drawing.Size(148, 22);
+            this.txtCuenta.TabIndex = 4;
+            // 
+            // txtSaldo
+            // 
+            this.txtSaldo.Location = new System.Drawing.Point(177, 224);
+            this.txtSaldo.Name = "txtSaldo";
+            this.txtSaldo.Size = new System.Drawing.Size(148, 22);
+            this.txtSaldo.TabIndex = 5;
+            // 
+            // txtDeposito
+            // 
+            this.txtDeposito.Location = new System.Drawing.Point(177, 362);
+            this.txtDeposito.Name = "txtDeposito";
+            this.txtDeposito.Size = new System.Drawing.Size(148, 22);
+            this.txtDeposito.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 368);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Deposito";
+            // 
+            // txtRetiro
+            // 
+            this.txtRetiro.Location = new System.Drawing.Point(592, 362);
+            this.txtRetiro.Name = "txtRetiro";
+            this.txtRetiro.Size = new System.Drawing.Size(148, 22);
+            this.txtRetiro.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(470, 368);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Retiro";
+            // 
+            // btnDeposito
+            // 
+            this.btnDeposito.Location = new System.Drawing.Point(592, 69);
+            this.btnDeposito.Name = "btnDeposito";
+            this.btnDeposito.Size = new System.Drawing.Size(112, 23);
+            this.btnDeposito.TabIndex = 10;
+            this.btnDeposito.Text = "Deposito";
+            this.btnDeposito.UseVisualStyleBackColor = true;
+            this.btnDeposito.Click += new System.EventHandler(this.btnDeposito_Click);
+            // 
+            // btnRetiro
+            // 
+            this.btnRetiro.Location = new System.Drawing.Point(592, 152);
+            this.btnRetiro.Name = "btnRetiro";
+            this.btnRetiro.Size = new System.Drawing.Size(118, 23);
+            this.btnRetiro.TabIndex = 11;
+            this.btnRetiro.Text = "Retiro";
+            this.btnRetiro.UseVisualStyleBackColor = true;
+            this.btnRetiro.Click += new System.EventHandler(this.btnRetiro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRetiro);
+            this.Controls.Add(this.btnDeposito);
+            this.Controls.Add(this.txtRetiro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDeposito);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSaldo);
+            this.Controls.Add(this.txtCuenta);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.TextBox txtCuenta;
+        private System.Windows.Forms.TextBox txtSaldo;
+        private System.Windows.Forms.TextBox txtDeposito;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtRetiro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDeposito;
+        private System.Windows.Forms.Button btnRetiro;
+    }
+}
+
---
+++ Evento/Evento/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evento
+{
+    public partial class Form1 : Form
+    {
+        public delegate void CambiarSaldoEventHandler(string strCliente, string strCuenta, double dblSaldo);
+        public event CambiarSaldoEventHandler CambioSaldo;
+
+        double valor;
+        public Form1()
+        {
+            InitializeComponent();
+            this.CambioSaldo += MetodoGestor;
+        }
+
+
+        public void Depositar(double dblCantidad)
+        {
+            if(dblCantidad > 0)
+            {
+                valor = double.Parse(txtSaldo.Text) + dblCantidad;
+                CambioSaldo(txtCliente.Text, txtCuenta.Text,valor);
+
+            }
+            else
+            {
+                MessageBox.Show("Cantidad Invalida");
+            }
+        }
+
+
+        public void Retirar(double dblCantidad)
+        {
+            if(double.Parse(txtSaldo.Text) >= dblCantidad)
+            {
+                valor = double.Parse(txtSaldo.Text) - dblCantidad;
+                CambioSaldo(txtCliente.Text, txtCuenta.Text, valor);
+
+            }
+            else
+            {
+                MessageBox.Show("Saldo Insuficiente");
+            }
+        }
+
+        public void MetodoGestor(string strCliente, string strCuenta, double dblSaldo)
+        {
+            MessageBox.Show("Estimado(a)" + strCliente + ":\nSe le notifica que el saldo de su cuenta"
+                +strCuenta + "se ha modificado\nNuevo Saldo:" + dblSaldo.ToString("C"));
+        }
+
+        private void btnDeposito_Click(object sender, EventArgs e)
+        {
+            Depositar(double.Parse(txtDeposito.Text));
+        }
+
+        private void btnRetiro_Click(object sender, EventArgs e)
+        {
+            Retirar(double.Parse(txtRetiro.Text));
+        }
+    }
+}
